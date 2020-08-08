e50b704681123e83cc5e28941dd07eb655cc1376
seth skinner <sethwskinner@gmail.com>
2020-08-08 16:43:28 -0500

    fixing functionality

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,8 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false
+CREATE TABLE burgers (
+id INT AUTO_INCREMENT NOT NULL,
+description VARCHAR(255),
+createdAt TIMESTAMP NOT NULL,
+PRIMARY KEY(id)
 );
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+use db burger_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Were Here Were Gruyere, Get Used to It Burger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("One Horse Open Slaw", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("I Dont Bay Leaf in Magic Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hit Me With Your Best Shallot Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Jack-O-Lentil Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("baby you can chive my car burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Cauliflowers Cumin From Inside the House Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Gourdon-Hamsey Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Use It or Bleus It Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Say It Aint Cilantro Burger", 0);
+
+SELECT * FROM burgers;
