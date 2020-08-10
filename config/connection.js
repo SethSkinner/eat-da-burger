3b31e5c30acaad4b9a28460625c8b52e9a2b1edd
seth skinner <sethwskinner@gmail.com>
2020-08-10 13:17:52 -0500

    pushing connections to servers

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+
+var connection;
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user:'root',
+    password: '',
+    database: 'burgers_db'
+  });
+};
+
+connection.connect(err => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadID);
+});
+
+module.exports = connection;
