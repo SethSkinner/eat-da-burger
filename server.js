a937d3e6c08bbcba80ab9fe56ac7f7d0f4d3b260
seth skinner <sethwskinner@gmail.com>
2020-08-10 14:14:23 -0500

    fixing connections

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-var express =require('express');
+var express = require("express");
+
 var PORT = process.env.PORT || 8080;
+
 var app = express();
 
-app.use(express.static('public'));
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var exphbs = require('express-handlebars');
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
-}));
-app.set('vie engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-var router = require('./controllers/burgers_controllers.js');
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_Controllers.js");
 
-app.use(router);
+app.use(routes);
 
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  console.log('server listening on: ' + PORT)
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
