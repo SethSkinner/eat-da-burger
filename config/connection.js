d37fc228722660ec36d130c5578c33f9b53cbeff
seth skinner <sethwskinner@gmail.com>
2020-08-10 16:34:08 -0500

    fixing connection

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,8 @@ var mysql = require('mysql');
 
 var connection;
 if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
   connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -9,14 +11,10 @@ if(process.env.JAWSDB_URL) {
     password: '',
     database: 'burgers_db'
   });
-};
+}
 
-connection.connect(err => {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadID);
-});
+
+
+connection.connect();
 
 module.exports = connection;
