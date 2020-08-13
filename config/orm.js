393f046baa0083c420ce626baa8c5031fffb8a15
Seth Skinner <63024867+SethSkinner@users.noreply.github.com>
2020-08-13 09:01:28 -0500

    Update orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,5 @@
 var connection = require("../config/connection.js");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
 var orm = {
   selectAll: function(tableInput, cb) {
     var queryString = "SELECT * FROM ??";
