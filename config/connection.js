1fe48ad658405c2a77f9a0cd1d6bdb09ef36b039
seth skinner <sethwskinner@gmail.com>
2020-08-27 17:26:37 -0500

    fixing connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 var mysql = require("mysql");
 
-var connection =
-  mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else { 
+  connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burger_db"
+    });
+};
 
 connection.connect(err => {
   if (err) {
