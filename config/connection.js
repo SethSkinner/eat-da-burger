079c5c51fee5b553d3958322ee4254b2585167f0
seth skinner <sethwskinner@gmail.com>
2020-08-27 17:43:59 -0500

    messing with connection

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,15 @@ var mysql = require("mysql");
 var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else { 
+} else {
   connection = mysql.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burger_db"
-    });
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "",
+      database: "burgers_db"
+  });
 };
-
 connection.connect(err => {
   if (err) {
     console.error("error connecting: " + err.stack);
