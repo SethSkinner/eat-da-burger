bfc3aeb823a79bb70f62160f577e2832700a782f
seth skinner <sethwskinner@gmail.com>
2020-08-27 17:22:57 -0500

    fixing indentation

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,6 @@
 var mysql = require("mysql");
 
-var connection;
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else { 
+var connection =
   mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -11,7 +8,6 @@ if (process.env.JAWSDB_URL) {
   password: "",
   database: "burgers_db"
 });
-};
 
 connection.connect(err => {
   if (err) {
--- config/orm.js
+++ config/orm.js
@@ -1,47 +1,44 @@
 var connection = require("../config/connection.js");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
 var orm = {
+
   selectAll: function(tableInput, cb) {
+    
     var queryString = "SELECT * FROM ??";
-    connection.query(queryString, [tableInput], function(err, result) {
-      if (err) throw err;
-      console.log(result);
-      cb(result);
-    });
-  },
-  insertOne: function(tableInput, cols, vals, cb) {
-
-    var queryString = "INSERT INTO ?? (??, ??) VALUES (?, ?)";
-    console.log(queryString);
-    connection.query(queryString, [tableInput, cols[0], cols[1], vals[0], vals[1]], function(err, result) {
-      if (err) throw err;
-      console.log(result);
-      cb(result);
-    });
-  },
-  updateOne: function(tableInput, col1, val1, id, cb) {
-    var queryString =
-      "UPDATE ?? set ?? = ? where id = ?";
-
-    connection.query(
-      queryString, [tableInput, col1, val1, id], function(err, result) {
+      connection.query(queryString, [tableInput], function(err, result) {
         if (err) throw err;
         console.log(result);
         cb(result);
-      }
-    );
-  },
-  deleteOne: function(tableInput, id, cb) {
-    var queryString =
-      "DELETE from ?? where id = ?";
+  });
+},
+  
+insertOne: function(tableInput, cols, vals, cb) {
 
-    connection.query(
-      queryString, [tableInput, id], function(err, result) {
+    var queryString = "INSERT INTO ?? (??, ??) VALUES (?, ?)";
+      console.log(queryString);
+      connection.query(queryString, [tableInput, cols[0], cols[1], vals[0], vals[1]], function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        cb(result);
+  });
+},
+  
+  updateOne: function(tableInput, col1, val1, id, cb) {
+    
+    var queryString = "UPDATE ?? set ?? = ? where id = ?";
+      connection.query(queryString, [tableInput, col1, val1, id], function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        cb(result);
+    }
+  );
+},
+
+  deleteOne: function(tableInput, id, cb) {
+    
+    var queryString = "DELETE from ?? where id = ?";
+
+    connection.query(queryString, [tableInput, id], function(err, result) {
         if (err) throw err;
         console.log(result);
         cb(result);
